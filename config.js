ca19dd8b10db2feb5f598b45f2747c0f65ed9bc0
boizdev <101125502+boizdev@users.noreply.github.com>
2022-03-07 23:40:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = ' Gửi Em';
+const contentCard = 'Chúc em 8-3 tràn ngập niềm vui và những nụ cười. Mong những điều tốt đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'mph.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
