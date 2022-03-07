970606057078eca15b1336757cba2a963ef2da78
boizdev <101125502+boizdev@users.noreply.github.com>
2022-03-08 05:34:37 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 8-3';
-const titleCard = ' Gửi Em';
-const contentCard = 'Chúc em 8-3 tràn ngập niềm vui và những nụ cười. Mong những điều tốt đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
-const giftImage = 'mph.png';
-const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
